030a6ea8c90e4036275efbf174f72b96324cd1a9
eynimeni <maganatte@yahoo.de>
2022-11-08 19:11:01 +0100

    empty project initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!--HelloWorld msg="Welcome to Your Vue.js App"/-->
+
   <ExerciseOne></ExerciseOne>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import ExerciseOne from "@/components/ExerciseOne";
+
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld
-    ExerciseOne
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
