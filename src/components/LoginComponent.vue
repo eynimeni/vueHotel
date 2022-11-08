d24e903fe423174ddc5cb527ddc47dc0818baabf
eynimeni <maganatte@yahoo.de>
2022-11-08 20:52:25 +0100

    login site dummy

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,14 +1,36 @@
 <template>
   <header-component></header-component>
-  <div>login</div>
+  <p>login</p>
+  <div>
+    <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else.">
+
+      <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+      ></b-form-input>
+    </b-form-group>
+
+  </div>
 </template>
 
 <script>
 import HeaderComponent from "@/components/subComponents/HeaderComponent";
 export default {
   name: "LoginComponent",
-  components: {HeaderComponent}
-}
+  components: {HeaderComponent},
+  data() {
+    return {
+      form: {
+        email: '',
+      }
+    }},}
 </script>
 
 <style scoped>
